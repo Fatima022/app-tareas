799d8c017cff26f0f74fd4f0302c65456c319b63
unknown <fatimarivera.226.fr@gmail.com>
2021-09-29 20:52:03 -0500

    Actualizacion 2

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //Con esto solo aquel que este autentificadi puede ver
+    //estas vistas
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('tasks.index');
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show(Task $task)
+    {
+        //
+    }
+
+    
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
