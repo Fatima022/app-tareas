22af4071e0d8a7e6c4dbce4dc47bce88b7fee18a
unknown <fatimarivera.226.fr@gmail.com>
2021-10-06 20:54:28 -0500

    Actualizacion 3

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,1 +1,43 @@
-VISTA DE EDITAR
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">EDITAR TAREA</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{route ('tareas.update', 
+                        $task->id) }}">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+
+                        <div class="form-group">
+                            <label for="">Título de tarea</label>
+                            <input type="text" name="title" class="form-control" 
+                            value="{{ $task->title }}" required="">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="">Fecha de Entrega</label>
+                            <input type="date" name="deadline" class="form-control"
+                             value="{{ $task->deadline }}">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="">Descripción</label>
+                            <textarea name="description" class="form-control" rows="5">
+                                {{ $task->description }}
+                            </textarea>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Actualizar</button>
+                        <a href="{{ route('tareas.index') }}" class="btn btn-outline-dark">Cancelar</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,12 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Mis Tareas') }}</div>
 
                 <div class="card-body">
-                    <h1>Aquí va a ir un listado</h1>
+                    <a href=" {{route ('tareas.create') }} " class="btn btn-primary mb-3">Crear Nueva Tarea</a>
+
+                    <table class="table table-sm">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Título</th>
+                            <th scope="col">Fecha de Entrega</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($tasks as $task)
+                            <tr>
+                            <th scope="row">1</th>
+                            <td> {{ $task->title }} </td>
+                            <td> {{ $task->deadline }} </td>
+                            <td> {{ $task->description }} </td>
+                            <td>
+                                @if($task->is_complete == false)
+                                <span class="badge bg-warning">Pendientes</span>
+                                @else
+                                <span class="badge bg-warning">Completada</span>
+                                @endif
+                            </td>
+                            <td>
+                                @if($task->is_complete == false)
+                                <a href="{{ route('tareas.status' , $task->id)}}" 
+                                    class="btn btn-outline-success btn-sm">Completar</a>
+                                @endif
+                                <a href=" {{ route('tareas.edit', $task->id) }} " 
+                                    class="btn btn-outline-info btn-sm">Editar</a>
+                            </td>
+                            </tr>
+                            @endforeach
+                        </thead>
+                    </table>
                 </div>
             </div>
         </div>
